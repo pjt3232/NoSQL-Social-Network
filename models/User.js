a95d6b36579b6f0fd4ebe2896d7fa407d5e7292d
Tranel <tranel@Tranels-MacBook-Air.local>
2023-07-12 11:36:06 -0500

    Added match valid email regex (User model)

--- models/User.js
+++ models/User.js
@@ -11,7 +11,8 @@ const userSchema = new mongoose.Schema(
         email: {
             type: String,
             unique: true,
-            required: true
+            required: true,
+            match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
         },
         thoughts: [{
             type: mongoose.SchemaTypes.ObjectId,
